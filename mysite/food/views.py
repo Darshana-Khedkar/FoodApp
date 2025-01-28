7dfc627379ca5ce886bb1a0781b3e855d1e114dc
Darshana-Khedkar <khedkar440@gmail.com>
2025-01-29 01:14:42 +0530

    Login/Logout functionality with proper pages

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,20 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 
 from .forms import ItemForm
 from .models import Item
 
 
-# Create your views here.
 def index(request):
     item_list = Item.objects.all()
 
     context = {
-        'item_list':item_list,
+        'item_list': item_list,
 
     }
-    return render(request,'food/index.html',context)
+    return render(request, 'food/index.html', context)
 
+
+class IndexClassView(ListView):
+    model = Item;
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
 def item(request):
     return HttpResponse('<h1>This is AN ITEM VIEW</h1>')
 
@@ -26,6 +33,10 @@ def detail(request,item_id):
     }
     return render(request,'food/detail.html',context)
 
+
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
 def create_item(request):
     form = ItemForm(request.POST or None)
 
@@ -34,6 +45,16 @@ def create_item(request):
         return redirect('food:index')
     return render(request, 'food/item-form.html', {"form": form})
 
+
+class CreateItem(CreateView):
+    model = Item;
+    fields = ['item_name', 'item_desc', 'item_price', 'item_image']
+    template_name = 'food/item-form.html'
+
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+
+        return super().form_valid(form)
 
 def update_item(request, id):
     item = Item.objects.get(id=id)
